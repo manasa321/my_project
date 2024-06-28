82ab451ef4bd6e757421441877a0edff1c9e301c
manasa321 <manasakata02@gmail.com>
2024-06-28 23:00:33 +0530

    added my_project
    added my_project

---
+++ script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    let user = document.getElementById('user').value;
+    let password = document.getElementById('password').value;
+
+    let fail = 0;
+    const lis = [0,1,4];
+    for(let i=0;i<lis.length;i++){
+        if((user[lis[i]]<'a' || user[lis[i]]>'z') && (user[lis[i]]<'A' || user[lis[i]]>'Z')){
+            fail++;
+            alert("Invalid username");
+            break;
+        }
+    }
+    const lis2 = [2,3,5,6,7];
+    for(let i=0;i<lis2.length;i++){
+        if(user[lis2[i]]<'0' || user[lis2[i]]>'9'){
+            fail++;
+            alert("Invalid username");
+            break;
+        }
+    }
+
+    if(fail==0)
+        alert("Signing in");
+}
